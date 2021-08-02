15472263c051e9ce81ee9c75c44691ea17caf6f8
Simon Smith <simonsmith5521@gmail.com>
2021-08-02 15:33:32 +0100

    multi platform and version

--- build.js
+++ build.js
@@ -1,11 +1,10 @@
 const { resolve } = require('path');
 const { compile } = require('nexe');
+const version = "14.17.4";
 (async function () {
   await compile({
     input: resolve(__dirname, 'index.js'),
-    output: resolve(__dirname, 'dist', 'windows-x64-14.17.4'),
-    targets: ['windows-x64-14.17.4'],
-    name: appName,
+    targets: [`windows-x64${version}`,`mac-x64${version}`,`linux-x64${version}`],
     build: true,
   });
 })().catch(console.error);
