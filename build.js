a20a17c841cddf7af7c3566731699411cec039cb
Simon Smith <simonsmith5521@gmail.com>
2021-08-02 15:50:17 +0100

    try 14 only for now

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 const { resolve } = require('path');
 const { compile } = require('nexe');
-const version = "14.17.4";
 (async function () {
   await compile({
     input: resolve(__dirname, 'index.js'),
-    targets: [`windows-x64${version}`,`mac-x64${version}`,`linux-x64${version}`],
+    targets: [`windows-x64`],
     build: true,
+    loglevel: 'verbose'
   });
 })().catch(console.error);
